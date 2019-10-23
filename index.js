64d0ee0401b243327e45b7bf3a19f61a109e0a1b
Agnes Font <agnesft5@gmail.com>
2019-10-23 20:58:53 +0200

    Añadidas funciones erase

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ function getUser(objJSON) {
     let userTitle = document.querySelector(".card__title");
     let cardBio = document.querySelector(".card__bio");
 
-    imgCard.src =objJSON["avatar_url"];
-    userCard.innerHTML =`@${objJSON["login"]}`;
-    userTitle.innerHTML =objJSON["name"];
+    imgCard.src = objJSON["avatar_url"];
+    userCard.innerHTML = `@${objJSON["login"]}`;
+    userTitle.innerHTML = objJSON["name"];
     cardBio.innerHTML = objJSON["bio"];
 
     let userLink = `https://api.github.com/users/${userName}/repos`;
@@ -55,11 +55,12 @@ function getLink() {
     httpGet(link, getUser)
 };
 
+
 function imprimir(objRepo) {
     console.log(objRepo);
     let likes = document.querySelector(".repo__likes");
     let forks = document.querySelector(".repo__forks");
-    for (i=0; i<objRepo.length;i++){
+    for (i = 0; i < objRepo.length; i++) {
         let repo = objRepo[i];
         let tr = `<tr><th scope="row" class="table__row">${repo["name"]}</th><td class="w-100 text-right"><i class="fas fa-star"></i><span class="repo__likes"> ${repo["watchers_count"]} </span><i class="fas fa-code-branch"></i><span class="repo__forks">${objRepo[0]["forks_count"]}</span></td></tr>`
 
@@ -68,16 +69,45 @@ function imprimir(objRepo) {
 
 };
 
-
-//////////////////// EVENT LISTENER /////////////////////
+/////////////// EVENT LISTENER //////////////////
+//////////////////// BOTÓ 1 /////////////////////
 
 
 let botonEnviar1 = document.querySelector(".send__button1");
 botonEnviar1.addEventListener("click", () => {
     getLink()
     let banner1 = document.querySelector(".banner1__body");
-    let banner2 = document.querySelector(".banner2__body");  
+    let banner2 = document.querySelector(".banner2__body");
     banner1.className = "banner banner1__body d-none";
     banner2.className = "banner banner2__body";
 });
 
+let botonEnviar3 = document.querySelector(".send__button3");
+botonEnviar3.addEventListener("click", () => {
+    let banner1 = document.querySelector(".banner1__body");
+    let banner3 = document.querySelector(".banner3__body");
+    banner1.className = "banner banner1__body";
+    banner3.className = "banner banner3__body d-none";
+});
+
+
+////////////////// BOTÓ 2 /////////////////////
+function ereseValue() {
+    let userNameInput = document.querySelector(".user__input1");
+    userNameInput.value = null;
+}
+
+function ereseData() {
+    document.querySelector("#tablaEspecial tbody").innerHTML = null;
+};
+
+
+let botonEnviar2 = document.querySelector(".send__button2");
+botonEnviar2.addEventListener("click", () => {
+    ereseValue();
+    let banner1 = document.querySelector(".banner1__body");
+    let banner2 = document.querySelector(".banner2__body");
+    banner1.className = "banner banner1__body";
+    banner2.className = "banner banner2__body d-none";
+    ereseData();
+});
